060248a1753c73c41f018691ac2090a178aabc53
petrenkopetro974 <106687742+petrenkopetro974@users.noreply.github.com>
2022-06-02 19:56:40 +0300

    Update Program.cs
    Добавлен комментарий в репозитории Dubler

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApp1
             // Комментарий 6
             // Комментарий 7
             // Добавлен комментарий 8
+            // Добавлен комментарий в репозитории Dubler
             Console.WriteLine("Hello World!");
         }
     }
