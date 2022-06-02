dbb35bfac44ac1bea666736b6ece42694e7a8b21
petrenkopetro974 <petrenkopetro974@gmail.com>
2022-06-02 16:24:28 +0300

    Фиксация последних изменений с помощью командной строки

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,8 @@ namespace ConsoleApp1
             // Комментарий 1
             // Комментарий 2
             // Комментарий 3
+            // Комментарий 4
+            // Комментарий 5
             Console.WriteLine("Hello World!");
         }
     }
